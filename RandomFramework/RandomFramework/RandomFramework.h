f2f7f3c27643a19ec35c6cf1da106e8f20a72845
Anoop Kumar <anoop.kumar@unthinkable.co>
2021-04-09 16:56:23 +0530

    Random Framework

---
+++ RandomFramework/RandomFramework/RandomFramework.h
@@ -0,0 +1,18 @@
+//
+//  RandomFramework.h
+//  RandomFramework
+//
+//  Created by daffolapmac146 on 09/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RandomFramework.
+FOUNDATION_EXPORT double RandomFrameworkVersionNumber;
+
+//! Project version string for RandomFramework.
+FOUNDATION_EXPORT const unsigned char RandomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomFramework/PublicHeader.h>
+
+
---
+++ RandomFramework/RandomFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  RandomFramework
+//
+//  Created by daffolapmac146 on 09/04/21.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Its a framework created to test the private pod"
+    }
+}
